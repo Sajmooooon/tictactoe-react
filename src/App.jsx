57e8f2dd3a24e758e619c08f052850f61ec0e622
Sajmooooon <sajmoooonsk@gmail.com>
2025-12-27 14:24:25 +0100

    feat: Vytvoreny komponent hraca a gameboardy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
+import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 function App() {
   
 
   return (
     <>
-    
+    <main>
+      <div id="game-container">
+        <ol id="players">
+
+          <Player initialName="Player 1" symbol="X"/>
+        </ol>
+
+        <GameBoard/>
+      </div>
+    </main>
     </>
   )
 }
---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+export default function Player({initialName, symbol, ...props}) {
+  const [editing, setEdit] = useState(false)
+
+  let [name, setName] = useState(initialName)
+
+  function handleEditClick () {
+    setEdit(editing => !editing)
+  }
+
+  let playerName = <span className="player-name">{name}</span>
+  let btnCaption = 'Edit'
+  
+  if (editing) {
+    playerName = <input type="textr" onChange={handleChange} required value={name}/>
+    btnCaption = "Save"
+  }
+
+  function handleChange(e) {
+    setName(e.target.value)
+  }
+
+
+  return (
+    <>
+    <li>
+      <span className="player">
+        {playerName}
+      
+      <span className="player-symbol">{symbol} {editing}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+    </>
+  )
+}
